054d07e34eae7d8f9f69171f2af3002271cf0a9b
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2023-09-10 17:12:36 -0400

    test

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { ProfileService } from './utils/ProfileService';
 
 export const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(express.raw({ type: 'application/vnd.custom-type' }));
 app.use(express.text({ type: 'text/html' }));
+const profileService = new ProfileService()
 
 // Healthcheck endpoint
 app.get('/', (req, res) => {
@@ -19,6 +21,15 @@ const api = express.Router();
 api.get('/hello', (req, res) => {
   res.status(200).send({ message: 'hello world' });
 });
+
+api.get('/profile', async (req, res) => {
+  if(req.headers.site!=='drama-freaks'){
+    res.status(400).send({})
+  }else {
+    res.status(200).send(profileService.getRandomProfile());
+  }
+});
+
 
 // Version the api
 app.use('/api/v1', api);
---
+++ src/utils/ProfileService.ts
@@ -0,0 +1,16 @@
+export class ProfileService{
+
+  /**
+   *
+   */
+  getRandomProfile(){
+
+    return {
+      name: 'John',
+      description: 'the fisherman',
+      photo_url: 'https://thispersondoesnotexist.com/'
+    }
+
+  }
+
+}
